2194a322d58113514aed8467172ad24bd13ce39e
Morty DDT <denis_denis_2001@yahoo.com>
2023-06-11 11:19:35 +0200

    Lastie Last

--- siw-movie-00/src/main/java/it/uniroma3/siw/model/Movie.java
+++ siw-movie-00/src/main/java/it/uniroma3/siw/model/Movie.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.CascadeType;
-import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
